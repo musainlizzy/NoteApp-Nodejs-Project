98b3eb3340a85c6cdd441c89052cf869c493dc30
Qi Liu <liu4@andrew.cmu.edu>
2017-10-30 20:58:44 -0400

    Implementd reading, adding, listing and removing.

--- app.js
+++ app.js
@@ -4,11 +4,41 @@ const fs = require('fs'); //fectch all the object in fs module
 const os = require('os');
 const notes = require('./notes.js');
 const _ = require('lodash');
-var filteredArray = _.uniq(['Evelyn',1,'Evelyn',1,2,2,3]);
+const yargs = require('yargs');
 
-console.log(_.uniq(filteredArray));
+const argv= yargs.argv;
+var command = process.argv[2];
+console.log('Command :',command);
 
-console.log('Result',notes.add(9,-2));
+if(command === 'add') {
+  console.log('adding new note');
+  var note = notes.addNote(argv.title,argv.body);
+  if (note) {
+    console.log('Note created');
+    notes.logNote(note);
+
+  }
+} else if (command ==='list'){
+  console.log('list all notes');
+  notes.getAll();
+
+} else if (command === 'read'){
+  console.log('read');
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log('Note found');
+    notes.logNote(note);
+  } else {
+    console.log('Note not found');
+  }
+} else if (command === 'remove'){
+  console.log('remove');
+  var remove = notes.removeNote(argv.title);
+  var message = remove? 'Note has been removed.':'Note can not be removed.';
+  console.log(message);
+} else {
+  console.log('Command is not recognized');
+}
 
 
 
--- notes.js
+++ notes.js
@@ -1,10 +1,64 @@
 console.log('starting notes.js');
+const fs = require('fs');
 
-module.exports.addNote = ()=>{
-  console.log('addNote');
-  return 'New notes'
+var fetchNotes =()=>{
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+
 };
 
-module.exports.add = (a,b)=>{
-  return a+b;
+var saveNotes=(notes)=>{
+  fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+};
+var addNote = (title, body) =>{
+  var notes =fetchNotes();
+  var note = {
+    title,
+    body
+  };
+
+  var duplicateNotes = notes.filter((note)=> note.title ===title);
+  if (duplicateNotes.length ===0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+
+
+};
+
+var getAll = () =>{
+
+};
+var getNote = (title) =>{
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note)=>note.title===title);
+  return filteredNotes[0];
+};
+var logNote = (note) =>{
+  console.log('--');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
 }
+var removeNote = (title) =>{
+  //fetch notes
+  // filter notes, removing the one with title of argument
+  //save new notes array
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note)=>note.title!=title);
+  saveNotes(filteredNotes);
+  return notes.length!=filteredNotes.length;
+
+
+};
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+  logNote
+};
